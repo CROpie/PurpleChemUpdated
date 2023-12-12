9daa451b22270a69f4e00950289d8298336a5ed9
CROpie <96667010+CROpie@users.noreply.github.com>
2023-12-13 00:43:53 +1100

    Changed to https

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,7 @@
-export const AuthURL = 'http://170.64.192.236:80'
-export const DataURL = 'http://170.64.192.236:85'
+// export const AuthURL = 'http://170.64.192.236:80'
+// export const DataURL = 'http://170.64.192.236:85'
+export const AuthURL = 'https://api1.cropie.store'
+export const DataURL = 'https://api2.cropie.store'
 export const CASURL = `https://commonchemistry.cas.org/api/detail`
 export const ChemNameURL = `https://rboq1qukh0.execute-api.us-east-2.amazonaws.com/default/`
 
