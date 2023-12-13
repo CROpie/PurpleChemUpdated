b89366b3bc98d5ea9f9501de5221d72534e91839
CROpie <96667010+CROpie@users.noreply.github.com>
2023-12-13 17:48:55 +1100

    revert

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        // Add any other external dependencies as needed
-        '/src/assets/@rdkit/rdkit/dist/RDKit_minimal.js',
-        '/src/assets/jsme-editor/jsme.nocache.js',
-      ],
-    },
-  },
 })
