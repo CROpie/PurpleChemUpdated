452bb58f1871c13cbe32e555b94aac1621ddb158
CROpie <96667010+CROpie@users.noreply.github.com>
2023-12-13 19:38:54 +1100

    increase chunksize limit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// export default defineConfig({
+//   plugins: [react()],
+
+// })
+
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      define: {
+        global: {},
+      },
+    }
+  }
+  if (command === 'build') {
+    return {
+      plugins: [react()],
+      build: {
+        chunkSizeWarningLimit: 1000,
+      },
+    }
+  }
 })
